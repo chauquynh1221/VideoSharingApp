a9ea2081e6a2304005faae9c43b66f8061729b64
chau <chau1282001@gmail.com>
2023-01-09 22:58:01 +0700

    new

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LinearProgress from '@mui/material/LinearProgress';
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import SignIn from "./pages/SignIn";
+import Search from "./pages/Search";
+import { useSelector } from "react-redux";
+import Channel from "./pages/Channel";
+import { SnackbarProvider} from 'notistack';
+import { fetchStart } from "./redux/videoSlice";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 20px;
+`;
+
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <SnackbarProvider>
+    <ThemeProvider theme={darkMode ?  lightTheme : darkTheme }>
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home type="random"/>} />
+                  <Route path="trends" element={<Home type="trend" />} />
+                  <Route path="subscriptions" element={<Home type="sub" />} />
+                  <Route path="search" element={<Search />} />
+                  <Route path="myChannel">
+                    <Route path=":id" element={<Channel />} />
+                  </Route>
+                  <Route
+                    path="signin"
+                    element={currentUser ? <Home type="random"/> : <SignIn />}
+                  />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
+    </SnackbarProvider>
+  );
+}
+
+export default App;
